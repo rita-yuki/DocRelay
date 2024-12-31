77cfd29a11bd6714cda3853add36156d083d4644
rita-snow <yummy.standrews@gmail.com>
2024-12-31 13:30:18 +0900

    ユーザー新規登録画面の実装

--- db/migrate/20241231032014_devise_create_users.rb
+++ db/migrate/20241231032014_devise_create_users.rb
@@ -4,8 +4,10 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
+      
       t.string :encrypted_password, null: false, default: ""
-
+      t.string :employee_number, null: false, unique: true
+      t.string :user_name
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
--- db/schema.rb
+++ db/schema.rb
@@ -13,13 +13,13 @@
 ActiveRecord::Schema[7.0].define(version: 2024_12_31_032014) do
   create_table "users", charset: "utf8", force: :cascade do |t|
     t.string "encrypted_password", default: "", null: false
+    t.string "employee_number", null: false
+    t.string "user_name"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "employee_number"
-    t.index ["employee_number"], name: "index_users_on_employee_number", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
