9b2bc73841d1142f66bad02c88e461724a7e386f
rita-snow <yummy.standrews@gmail.com>
2024-12-31 14:18:41 +0900

    home画面の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:employee_number, :name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:employee_number, :name])
+  end
+
+  def after_sign_in_path_for(resource)
+    root_path 
+  end
 end
