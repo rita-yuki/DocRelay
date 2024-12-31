2d4efbc26fb950c70a6a84a67b99f9a5c74a2e27
rita-snow <yummy.standrews@gmail.com>
2024-12-31 10:59:47 +0900

    render-build.shの設定の変更

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,6 +3,7 @@
 set -o errexit
 
 bundle install
-bundle exec rake assets:precompile
-bundle exec rake assets:clean
-bundle exec rake db:migrate
+RAILS_ENV=production bundle exec rake assets:clean
+RAILS_ENV=production bundle exec rake assets:precompile
+RAILS_ENV=production bundle exec rake db:migrate
+RAILS_ENV=production bundle exec rake tmp:cache:clear
