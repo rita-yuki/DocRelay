7180e9c4ab3af692b041f532368b1ae9ec2dda2d
rita-snow <yummy.standrews@gmail.com>
2025-01-02 15:35:13 +0900

    受領日・手続き開始日・手続き期日のバリテーションの設定

--- app/models/document.rb
+++ app/models/document.rb
@@ -4,6 +4,23 @@ class Document < ApplicationRecord
   belongs_to_active_hash :quantity
   belongs_to_active_hash :progress_status
   belongs_to :user
-  
+
   validates :received_date, :start_date, :due_date, :customer_name, presence: true
+  validate :start_date_and_due_date_validations
+
+  private
+
+  def start_date_and_due_date_validations
+    if start_date.present? && start_date < received_date
+      errors.add(:start_date, 'の日付を確認してください。')
+    end
+
+    if due_date.present? && due_date < start_date
+      errors.add(:due_date, 'の日付を確認してください。')
+    end
+
+    if due_date.present? && due_date < received_date
+      errors.add(:due_date, 'の日付を確認してください。')
+    end
+  end
 end
