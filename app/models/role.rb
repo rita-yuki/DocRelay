3db51670dc10ecc867a386b5a1cafb6be039694d
rita-snow <yummy.standrews@gmail.com>
2025-01-03 23:50:02 +0900

    roleモデルの修正

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,4 +1,4 @@
-class Role < ApplicationRecord
+class Role <  ActiveHash::Base
   self.data = [
     { id: 1, name: '管理者' },
     { id: 2, name: '担当者' }
@@ -6,5 +6,4 @@ class Role < ApplicationRecord
 
   include ActiveHash::Associations
   has_many :users
-end
 end
