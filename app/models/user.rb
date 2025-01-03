6977ba7838705d8bb3a8b4bc5c13a56b5c5663e2
rita-snow <yummy.standrews@gmail.com>
2025-01-03 23:50:36 +0900

    userモデルの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,12 @@ class User < ApplicationRecord
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
- validates :employee_number, presence: true, uniqueness: true
- :role_id, presence: true, inclusion: { in: Role.pluck(:id), message: "は選択肢の中から選んでください。" }
- before_update :prevent_employee_number_change
- before_validation :set_default_role, on: :create
+
+  validates :employee_number, presence: true, uniqueness: true
+  validates :role_id, presence: true, inclusion: { in: -> { Role.pluck(:id) }, message: "は選択肢の中から選んでください。" }
+
+  before_update :prevent_employee_number_change
+  before_validation :set_default_role, on: :create
 
   # Emailの検証を無効化
   def email_required?
