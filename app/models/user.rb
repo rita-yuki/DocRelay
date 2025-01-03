57a8a118ea3154bd5366ded2a9b6b44999064b90
rita-snow <yummy.standrews@gmail.com>
2025-01-03 23:36:06 +0900

    userモデルにアソシエーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,14 @@
 class User < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :role
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
  validates :employee_number, presence: true, uniqueness: true
-
+ :role_id, presence: true, inclusion: { in: Role.pluck(:id), message: "は選択肢の中から選んでください。" }
  before_update :prevent_employee_number_change
+ before_validation :set_default_role, on: :create
+
   # Emailの検証を無効化
   def email_required?
     false
@@ -25,5 +30,9 @@ class User < ApplicationRecord
       errors.add(:employee_number, "は変更できません。")
       throw(:abort)
     end
+  end
+
+  def set_default_role
+    self.role_id ||= Role.find_by(name: '担当者')&.id
   end
 end
