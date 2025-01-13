734abadab8d9aa58d9128eb2422cb8531e353ac9
rita-snow <yummy.standrews@gmail.com>
2025-01-13 13:04:19 +0900

    コントローラーの設定

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,12 +1,27 @@
-class Users::RegistrationsController < ApplicationController
+class Users::RegistrationsController < Devise::RegistrationsController
   before_action :configure_account_update_params, only: [:update]
-end
 
-def configure_sign_up_params
+ def configure_sign_up_params
   devise_parameter_sanitizer.permit(:sign_up, keys: [:employee_number, :user_name, :role_id])
-end
+ end
 
-def configure_account_update_params
+ def configure_account_update_params
   devise_parameter_sanitizer.permit(:account_update, keys: [:role, :password, :password_confirmation, :current_password, :role_id])
-end
+ end
+
+
+ def update_resource(resource, params)
+  if params[:password].blank? && params[:password_confirmation].blank?
+    resource.update_without_password(params.except(:current_password))
+  else
+    super
+  end
+
+  def after_update_path_for(resource)
+    root_path
+  end
+
+ def edit
+  super
+ end
 end
