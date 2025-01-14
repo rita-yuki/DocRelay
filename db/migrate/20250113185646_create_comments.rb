61bd81d88a91fd2d19a8ac2555305bbccdbce89b
rita-snow <yummy.standrews@gmail.com>
2025-01-14 09:17:14 +0900

    マイグレーションファイルの編集

--- db/migrate/20250113185646_create_comments.rb
+++ db/migrate/20250113185646_create_comments.rb
@@ -1,7 +1,7 @@
 class CreateComments < ActiveRecord::Migration[7.0]
   def change
     create_table :comments do |t|
-      t.text :content
+      t.string :text, null: false 
       t.references :user, null: false, foreign_key: true
       t.references :document, null: false, foreign_key: true
 
