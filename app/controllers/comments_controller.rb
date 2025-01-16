eac095a38ba9f702174b7f54f219fbe26a39d8bd
rita-snow <yummy.standrews@gmail.com>
2025-01-17 06:02:51 +0900

    commentsのindexビュー作成

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,14 +1,19 @@
 class CommentsController < ApplicationController
+  before_action :set_document, only: [:index, :create]
+
+  def index
+    @comments = @document.comments.includes(:user).order(created_at: :desc)
+  end
+
   def create
-    @document = Document.find(params[:document_id])
     @comment = @document.comments.new(comment_params)
     @comment.user = current_user
 
     if @comment.save
-      redirect_to document_path(@document), notice: '投稿されました。'
+      redirect_to document_comments_path(@document), notice: '投稿されました。'
     else
       flash[:alert] = '投稿に失敗しました。'
-      redirect_to document_path(@document)
+      redirect_to document_comments_path(@document)
     end
   end
 
@@ -17,5 +22,4 @@ class CommentsController < ApplicationController
   def comment_params
     params.require(:comment).permit(:content)
   end
-end
 end
