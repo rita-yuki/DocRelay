b82f8c3a07626fb64d0c2590d820699eb6797771
rita-snow <yummy.standrews@gmail.com>
2025-01-20 05:01:13 +0900

    commecontentcontentカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2025_01_13_185646) do
+ActiveRecord::Schema[7.0].define(version: 2025_01_19_195909) do
   create_table "comments", charset: "utf8", force: :cascade do |t|
     t.string "text", null: false
     t.bigint "user_id", null: false
     t.bigint "document_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "content"
     t.index ["document_id"], name: "index_comments_on_document_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
