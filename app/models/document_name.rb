b47b94a3105d219bb339b537fd2ddd7e178576fb
rita-snow <yummy.standrews@gmail.com>
2025-01-26 16:43:26 +0900

    document_name.rbの修正

--- app/models/document_name.rb
+++ app/models/document_name.rb
@@ -1,10 +1,11 @@
 class DocumentName < ActiveHash::Base
+  include ActiveHash::Associations
+  has_many :documents
+  validates :name, presence: true
   self.data = [
     { id: 1, name: '契約書' },
     { id: 2, name: '同意書' },
     { id: 3, name: '申込書' },
     { id: 4, name: '本人確認資料' }
   ]
-  include ActiveHash::Associations
-  has_many :documents
 end
