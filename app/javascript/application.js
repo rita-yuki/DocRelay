3b3365d82ebd7a856e25b8dc4812dc7bd338cab2
rita-snow <yummy.standrews@gmail.com>
2025-01-28 09:18:04 +0900

    モジュールとして認識できるよう修正

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,7 @@
-import Rails from "@rails/ujs";
-Rails.start();
-
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails";
 import "controllers";
+
+// @rails/ujsのインポートを修正
+import Rails from "@rails/ujs";
+Rails.start();
